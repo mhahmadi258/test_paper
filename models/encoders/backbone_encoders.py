e77ee520af48e0e22aceb3d3adce680e7df68a61
Mohammad Hossein Ahmadi <mh.ahmadi258@gmail.com>
2024-12-04 16:59:48 +1100

    psp

--- models/encoders/backbone_encoders.py
+++ models/encoders/backbone_encoders.py
@@ -1,25 +1,36 @@
+import numpy as np
 import torch
 import torch.nn.functional as F
 from torch import nn
 from torch.nn import Linear, Conv2d, BatchNorm2d, PReLU, Sequential, Module
 
 from models.encoders.helpers import get_blocks, Flatten, bottleneck_IR, bottleneck_IR_SE
-    
-class AdapterBlock(Module):
-    def __init__(self, in_channel, num_module):
-        super().__init__()
-        self.num_module = num_module
-        self.adapter = Sequential(BatchNorm2d(in_channel),
-                                         torch.nn.AdaptiveAvgPool2d((7, 7)),
-                                         Flatten(),
-                                         Linear(in_channel * 7 * 7, 2 * in_channel),
-                                         nn.GELU(),
-                                         Linear(2 * in_channel, 512 * num_module))
-        
+from models.stylegan2.model import EqualLinear
 
-    def forward(self, x , w):
-        out = self.adapter(x).view(-1, self.num_module, 512)
-        return w + out
+
+class GradualStyleBlock(Module):
+    def __init__(self, in_c, out_c, spatial):
+        super(GradualStyleBlock, self).__init__()
+        self.out_c = out_c
+        self.spatial = spatial
+        num_pools = int(np.log2(spatial))
+        modules = []
+        modules += [Conv2d(in_c, out_c, kernel_size=3, stride=2, padding=1),
+                    nn.LeakyReLU()]
+        for i in range(num_pools - 1):
+            modules += [
+                Conv2d(out_c, out_c, kernel_size=3, stride=2, padding=1),
+                nn.LeakyReLU()
+            ]
+        self.convs = nn.Sequential(*modules)
+        self.linear = EqualLinear(out_c, out_c, lr_mul=1)
+
+    def forward(self, x):
+        x = self.convs(x)
+        x = x.view(-1, self.out_c)
+        x = self.linear(x)
+        return x
+
 
 class EfficientEncoder(Module):
     def __init__(self, num_layers, mode='ir', opts=None):
@@ -34,28 +45,6 @@ class EfficientEncoder(Module):
         self.input_layer = Sequential(Conv2d(opts.input_nc, 64, (3, 3), 1, 1, bias=False),
                                       BatchNorm2d(64),
                                       PReLU(64))
-
-        self.output_layer_3 = Sequential(BatchNorm2d(256),
-                                         torch.nn.AdaptiveAvgPool2d((7, 7)),
-                                         Flatten(),
-                                         Linear(256 * 7 * 7, 512 * 9))
-        
-        # self.adapter_layer_3 = AdapterBlock(256, 9)
-        
-        self.output_layer_4 = Sequential(BatchNorm2d(128),
-                                         torch.nn.AdaptiveAvgPool2d((7, 7)),
-                                         Flatten(),
-                                         Linear(128 * 7 * 7, 512 * 5))
-        
-        # self.adapter_layer_4 = AdapterBlock(128, 5)
-        
-        self.output_layer_5 = Sequential(BatchNorm2d(64),
-                                         torch.nn.AdaptiveAvgPool2d((7, 7)),
-                                         Flatten(),
-                                         Linear(64 * 7 * 7, 512 * 4))
-        
-        # self.adapter_layer_5 = AdapterBlock(64, 4)
-        
         modules = []
         for block in blocks:
             for bottleneck in block:
@@ -63,22 +52,65 @@ class EfficientEncoder(Module):
                                            bottleneck.depth,
                                            bottleneck.stride))
         self.body = Sequential(*modules)
-        self.modulelist = list(self.body)
+
+        self.styles = nn.ModuleList()
+        self.style_count = opts.n_styles
+        self.coarse_ind = 3
+        self.middle_ind = 7
+        for i in range(self.style_count):
+            if i < self.coarse_ind:
+                style = GradualStyleBlock(512, 512, 16)
+            elif i < self.middle_ind:
+                style = GradualStyleBlock(512, 512, 32)
+            else:
+                style = GradualStyleBlock(512, 512, 64)
+            self.styles.append(style)
+        self.latlayer1 = nn.Conv2d(256, 512, kernel_size=1, stride=1, padding=0)
+        self.latlayer2 = nn.Conv2d(128, 512, kernel_size=1, stride=1, padding=0)
+
+    def _upsample_add(self, x, y):
+        '''Upsample and add two feature maps.
+        Args:
+          x: (Variable) top feature map to be upsampled.
+          y: (Variable) lateral feature map.
+        Returns:
+          (Variable) added feature map.
+        Note in PyTorch, when input size is odd, the upsampled feature map
+        with `F.upsample(..., scale_factor=2, mode='nearest')`
+        maybe not equal to the lateral feature map size.
+        e.g.
+        original input size: [N,_,15,15] ->
+        conv2d feature map size: [N,_,8,8] ->
+        upsampled feature map size: [N,_,16,16]
+        So we choose bilinear upsample which supports arbitrary output sizes.
+        '''
+        _, _, H, W = y.size()
+        return F.interpolate(x, size=(H, W), mode='bilinear', align_corners=True) + y
 
     def forward(self, x):
         x = self.input_layer(x)
-        for l in self.modulelist[:3]:
-          x = l(x)
-        lc_part_4 = self.output_layer_5(x).view(-1, 4, 512)
-        # lc_part_4 = self.adapter_layer_5(x, lc_part_4)
-        for l in self.modulelist[3:7]:
-          x = l(x)
-        lc_part_3 = self.output_layer_4(x).view(-1, 5, 512)
-        # lc_part_3 = self.adapter_layer_4(x, lc_part_3)
-        for l in self.modulelist[7:21]:
-          x = l(x)
-        lc_part_2 = self.output_layer_3(x).view(-1, 9, 512)
-        # lc_part_2 = self.adapter_layer_3(x, lc_part_2)
 
-        x = torch.cat((lc_part_2, lc_part_3, lc_part_4), dim=1)
-        return x
+        latents = []
+        modulelist = list(self.body._modules.values())
+        for i, l in enumerate(modulelist):
+            x = l(x)
+            if i == 6:
+                c1 = x
+            elif i == 20:
+                c2 = x
+            elif i == 23:
+                c3 = x
+
+        for j in range(self.coarse_ind):
+            latents.append(self.styles[j](c3))
+
+        p2 = self._upsample_add(c3, self.latlayer1(c2))
+        for j in range(self.coarse_ind, self.middle_ind):
+            latents.append(self.styles[j](p2))
+
+        p1 = self._upsample_add(p2, self.latlayer2(c1))
+        for j in range(self.middle_ind, self.style_count):
+            latents.append(self.styles[j](p1))
+
+        out = torch.stack(latents, dim=1)
+        return out
